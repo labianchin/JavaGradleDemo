ed41660373fcf763b85bb9fac4902a99ebc83316
Luis Armando Bianchin <luis.armandob@gmail.com>
2014-03-19 00:34:37 -0300

    Adds lots of dependencies at pom.xml

--- src/test/java/com/l433/javademo/HelloWorldTest.java
+++ src/test/java/com/l433/javademo/HelloWorldTest.java
@@ -1,5 +1,6 @@
 package com.l433.javademo;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -12,5 +13,6 @@ public class HelloWorldTest {
         
         //NOTHING to test yet, so do a dummy assert
         assertEquals("com.l433.javademo.HelloWorld", helloWorld.getClass().getName());
+        assertThat(helloWorld.getClass().getName(), equalTo("com.l433.javademo.HelloWorld"));
     }
 }
